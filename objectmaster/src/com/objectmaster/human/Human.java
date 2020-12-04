61f75a15f7198ae29daa7804d9757167376b089c
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-04 08:57:26 -0800

    Created Ninja class (loadout for Human class) with unque methods, and made some adjustments to Human class mathods

--- objectmaster/src/com/objectmaster/human/Human.java
+++ objectmaster/src/com/objectmaster/human/Human.java
@@ -3,24 +3,31 @@ package com.objectmaster.human;
 public class Human {
 //	Attributes
 	public String name;
-	private int strength = 3;
-	private int stealth = 3;
-	private int intelligence = 3;
-	private int health = 100;
+	protected int health = 100;
+	public int strength = 3;
+	public int stealth = 3;
+	public int intelligence = 3;
 	
 //	Methods
 	public void nameHuman(String name) {
 		this.name = name;
 	}
 	public void stats() {
-		System.out.println("Name: " + this.name + " |Strength: " + this.strength + " |Stealth: " + this.stealth + " |Intelligence: " + this.intelligence + " |Health :" + this.health + "%");
+		System.out.println("Loadout: No class |Name: " + this.name + " |Strength: " + this.strength + " |Stealth: " + this.stealth + " |Intelligence: " + this.intelligence + " |Health :" + this.health + "%");
 	}
 	
-	private void lossingHealth(Human person, int num) {
+	public void gainHealth(Human person, int num) {
+		person.health += num;
+	}
+	private void lossHealth(Human person, int num) {
 		person.health -= num;
 		System.out.println(person.name + " was attacked by " + this.name + " and lost " + num + "% of Health!");
 	}
+	
 	public void attack(Human person) {
-		person.lossingHealth(person, this.strength);
+		person.lossHealth(person, this.strength);
+	}
+	protected void attack(Human person, int num) {
+		person.lossHealth(person, num);
 	}
 }
--- objectmaster/src/com/objectmaster/loadoutclasses/Ninja.java
+++ objectmaster/src/com/objectmaster/loadoutclasses/Ninja.java
@@ -1,5 +1,26 @@
 package com.objectmaster.loadoutclasses;
 
-public class Ninja {
+import com.objectmaster.human.Human;
 
+public class Ninja extends Human {
+	
+	public String loadout = "Ninja";
+	public void stats() {
+		System.out.println("Loadout: " + this.loadout + " |Name: " + this.name + " |Strength: " + this.strength + " |Stealth: " + this.stealth + " |Intelligence: " + this.intelligence + " |Health :" + this.health + "%");
+	}
+	
+	public Ninja() {
+		this.stealth = 10;
+	}
+	
+	public void steal(Human person) {
+		this.attack(person, this.stealth);
+		this.gainHealth(this, this.stealth);
+		System.out.println(this.name + " stole " + this.stealth + "% of health from " + person.name + "!");
+	}
+	
+	public void runAway() {
+		this.attack(this, this.stealth);
+		System.out.println(this.name + " ran away and lost " + this.stealth + "% of health!");
+	}
 }

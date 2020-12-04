386a4390a7ca310e22230c5f7fa7ef0e1f4433c6
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-03 22:45:53 -0800

    Created Human class with a few Member Variables and Methods

--- objectmaster/src/com/objectmaster/human/Human.java
+++ objectmaster/src/com/objectmaster/human/Human.java
@@ -1,5 +1,22 @@
 package com.objectmaster.human;
 
 public class Human {
-
+//	Attributes
+	public String name;
+	private int strength = 3;
+	private int stealth = 3;
+	private int intelligence = 3;
+	private int health = 100;
+	
+//	Methods
+	public Human(String name) {
+		this.name = name;
+	}
+	private void lossingHealth(Human person, int num) {
+		this.name = name;
+	}
+	
+	public void attack(Human person) {
+		person.lossingHealth(person, this.strength);
+	}
 }
